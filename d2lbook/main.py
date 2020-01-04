e14c1e0b020d132a85e8b0d2ec71bc9c6d907d45
Tiep Vu <vuhuutiep@gmail.com>
2020-01-04 13:09:45 -0800

    remove set defaultencoding

--- d2lbook/main.py
+++ d2lbook/main.py
@@ -7,14 +7,14 @@ import logging
 
 import sys
 # reload(sys)
-sys.setdefaultencoding('utf-8')
+# sys.setdefaultencoding('utf-8')
 
 logging.basicConfig(format='[d2lbook:%(filename)s:L%(lineno)d] %(levelname)-6s %(message)s')
 logging.getLogger().setLevel(logging.INFO)
 
 
 def main():
-    commands = {'build': build, 'deploy':deploy, 'clear':clear}
+    commands = {'build': build, 'deploy': deploy, 'clear': clear}
     parser = argparse.ArgumentParser(description='''
 D2L Book: Publish a book based on Jupyter notebooks.
 
@@ -23,6 +23,7 @@ Run d2lbook command -h to get the help message for each command.
     parser.add_argument('command', nargs=1, choices=commands.keys())
     args = parser.parse_args(sys.argv[1:2])
     commands[args.command[0]]()
+
 
 if __name__ == "__main__":
     main()
